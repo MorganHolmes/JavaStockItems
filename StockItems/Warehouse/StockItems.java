0b1a4005a6ecaad5efff72861ef47c94370cac50
Morgan Holmes <morgan.holmes@live.co.uk>
2019-10-10 15:00:31 +0100

    Completed Task 3.

--- StockItems/Warehouse/StockItems.java
+++ StockItems/Warehouse/StockItems.java
@@ -2,8 +2,52 @@ package Warehouse;
 
 public class StockItems
 {
+    //Private variables
     private String description;
     private int level;
     private double price;
     
-}
+    //Constructor
+    public StockItems(String desc, double price){
+        level = 0;
+        description = desc;
+        this.price = price;
+    }
+    
+    //Mutators
+    public void setPrice(double price){
+        this.price = price;
+    }
+    
+    public void doDelivery(int amount){
+        level += amount;
+    }
+    
+    public void doSale(int amount){
+        if(level < amount){
+            System.out.print("Not Enough Stock To Complete Sale");
+        }else{
+            level -= amount;
+        }
+    }
+    
+    public int getStockLevel(){
+        return level;
+    }
+    
+    public String getDescription(){
+        return description;
+    }
+    
+    public double getPrice(){
+        return price;
+    }
+    
+    public String getStockInfo(){
+        return "*** Stock Report ***\n" + "Item: " + getDescription()
+        + "\nPrice: " + getPrice() + "\nStock Level: " + getStockLevel() +
+        "\n********************";
+    }
+    }
+    
+
--- StockItems/Warehouse/Tester.java
+++ StockItems/Warehouse/Tester.java
@@ -1,35 +1,9 @@
 package Warehouse;
-
-
-/**
- * Write a description of class Tester here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Tester
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Tester
-     */
-    public Tester()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+   public static void main(String[] args){
+       StockItems test = new StockItems("Baked Beans 415g",65);
+       //System.out.println("Done");
+       System.out.println(test.getStockInfo());
     }
 }
